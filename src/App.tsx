f1a80a3e47af88d7e00c16838b3f942031590921
achiewe <mamukashviliachi706@gmail.com>
2023-05-14 14:18:33 +0400

    add app tsx main page styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import GlobalStyles from "./GlobalStyles";
 import styled from "styled-components";
+import MobBg from "../src/images/bg-intro-mobile.png";
+
 function App(): JSX.Element {
-  return <GlobalStyles />;
+  return (
+    <StyledMain>
+      <GlobalStyles />
+    </StyledMain>
+  );
 }
 
-const styledMain = styled.div`
+const StyledMain = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background-image: url(${MobBg});
+  background-repeat: no-repeat;
+  background-size: 100% 100vh;
+  padding: 88px 24px 68px 24px;
+  background-color: #ff7979;
 `;
 export default App;
