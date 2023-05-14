6e488c729be5fa18166ecf11fd5fbc6dfc5b5e23
achiewe <mamukashviliachi706@gmail.com>
2023-05-14 14:49:36 +0400

    add PricingBanner component and style it

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,26 @@ import GlobalStyles from "./GlobalStyles";
 import styled from "styled-components";
 import MobBg from "../src/images/bg-intro-mobile.png";
 import PageHeader from "./components/PageHeader";
+import PricingBanner from "./components/PricingBanner";
 
 function App(): JSX.Element {
   return (
-    <StyledMain>
+    <StyledMainContainer>
       <GlobalStyles />
       <PageHeader />
-    </StyledMain>
+      <PricingBanner />
+    </StyledMainContainer>
   );
 }
 
-const StyledMain = styled.div`
+const StyledMainContainer = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 64px;
   background-image: url(${MobBg});
   background-repeat: no-repeat;
   background-size: 100% 100vh;
---
+++ src/components/PricingBanner.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const PricingBanner = (): JSX.Element => {
+  return (
+    <BannerDiv>
+      <p>
+        Try it free 7 days <span>then $20/mo. thereafter </span>
+      </p>
+    </BannerDiv>
+  );
+};
+
+const BannerDiv = styled.div`
+  width: 327px;
+  padding: 18px 67px;
+  background-color: #5e54a4;
+  border-radius: 10px;
+  box-shadow: 0px 8px 0px rgba(0, 0, 0, 0.14688);
+
+  p {
+    font-size: 15px;
+    font-weight: 700;
+    line-height: 26px;
+    letter-spacing: 0.2678571045398712px;
+    text-align: center;
+    color: #ffffff;
+
+    span {
+      font-size: 15px;
+      font-weight: 400;
+      line-height: 26px;
+      letter-spacing: 0.2678571045398712px;
+      text-align: center;
+    }
+  }
+`;
+export default PricingBanner;
